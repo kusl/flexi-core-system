810301c1add7a8c3cd16369f6566f5a3a3af8ab4
Allan Callaghan <calaldees@hotmail.com>
2013-10-04 19:26:29 +0100

    Updated tests to check all links are valid This test is temporerally disabled until the pages can be tidyed

--- webapp/flexi/tests/test_cache_manifest.py
+++ webapp/flexi/tests/test_cache_manifest.py
@@ -1,6 +1,8 @@
 import re
 from collections import defaultdict
 
+from bs4 import BeautifulSoup
+
 
 def test_cache_manifest(app):
     """
@@ -27,10 +29,36 @@ def test_cache_manifest(app):
     assert len(files['CACHE:']) > 20
     
     # Load each file in cache manifest
+    visited_urls = set()
+    links_to_check = set()
     for item in files['CACHE:']:
         if not item.startswith('/'):
             item = '/{0}'.format(item)
         response = app.get(item)
         assert response.status_code == 200
+        visited_urls.add(item)
+        relative_path = re.sub(r'(.*)/.*', r'\1', item)
+        
+        # Extract all links/images from pages visited in cache manifest
+        if 'html' in response.content_type:
+            soup = BeautifulSoup(response.text)
+            for tag, attr in (('a','href'),('img','src')):
+                for link in (t.get(attr) for t in soup.findAll(tag)):
+                    if link and not link.startswith('#') and not link.startswith('http:'):
+                        # Correct relative links to page links
+                        if not link.startswith('/'):
+                            link = "{0}/{1}".format(relative_path,link)
+                        links_to_check.add(link)
+    
+    links_to_check = links_to_check - visited_urls
 
-        # TODO - if content type = html -> beautiful soup each page and follow all relative links
+    # links_to_check are links on our pages that are not accounted for.
+    # We may have links to external resorces that are not tracked by cache.manifest (e.g offline links)
+    # However, these could be link errors. Try the link, report if the link does not exisit
+    for link in links_to_check:
+        try:
+            response = app.get(link)
+        except Exception:
+            response = None
+        # Temp removed until the site is tidyed up
+        #assert response and response.status_code == 200, 'This project has a page linking to "{0}" but the page dose not exist.'.format(link)
